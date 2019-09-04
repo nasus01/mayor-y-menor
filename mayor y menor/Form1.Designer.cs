17c450193537c6bcf8204f1116c4a9918d9ab03f
cristian ermes morales loaiza <christianjack01@hotmail.com>
2019-09-03 22:52:27 -0500

    identifica cual es mayor y menor

---
+++ mayor y menor/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace mayor_y_menor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblmayor = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblmenor = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.btnactivar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "identifica mayor y menor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "segundo numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "numero mayor es:";
+            // 
+            // lblmayor
+            // 
+            this.lblmayor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblmayor.Location = new System.Drawing.Point(193, 316);
+            this.lblmayor.Name = "lblmayor";
+            this.lblmayor.Size = new System.Drawing.Size(82, 27);
+            this.lblmayor.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(369, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "numero menor es:";
+            // 
+            // lblmenor
+            // 
+            this.lblmenor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblmenor.Location = new System.Drawing.Point(581, 313);
+            this.lblmenor.Name = "lblmenor";
+            this.lblmenor.Size = new System.Drawing.Size(90, 30);
+            this.lblmenor.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(18, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(135, 24);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "primer numero";
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Location = new System.Drawing.Point(196, 111);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero1.TabIndex = 1;
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Location = new System.Drawing.Point(196, 148);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero2.TabIndex = 1;
+            // 
+            // btnactivar
+            // 
+            this.btnactivar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnactivar.Location = new System.Drawing.Point(460, 132);
+            this.btnactivar.Name = "btnactivar";
+            this.btnactivar.Size = new System.Drawing.Size(75, 50);
+            this.btnactivar.TabIndex = 2;
+            this.btnactivar.Text = "activar";
+            this.btnactivar.UseVisualStyleBackColor = true;
+            this.btnactivar.Click += new System.EventHandler(this.Btnactivar_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(596, 132);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(84, 50);
+            this.btnlimpiar.TabIndex = 3;
+            this.btnlimpiar.Text = "limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.Btnlimpiar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(541, 186);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(81, 45);
+            this.btnsalir.TabIndex = 4;
+            this.btnsalir.Text = "salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.Btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btnactivar);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.lblmenor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblmayor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblmayor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblmenor;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.Button btnactivar;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+    }
+}
+
---
+++ mayor y menor/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayor_y_menor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnsalir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("gracias por utilizar mi programa--att:jack01");
+            this.Close();
+            
+        }
+
+        private void Btnlimpiar_Click(object sender, EventArgs e)
+        {
+            lblmayor.Text = "";
+            lblmenor.Text = "";
+            txtnumero1.Text = "";
+            txtnumero2.Text = "";
+        }
+
+        private void Btnactivar_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            num1 = int.Parse(txtnumero1.Text);
+            num2 = int.Parse(txtnumero2.Text);
+            if (num1 > num2)
+            {
+                lblmayor.Text = num2.ToString();
+                lblmenor.Text = num1.ToString();
+                lblmayor.Visible = true;
+                lblmenor.Visible = true;
+
+            }
+            else
+               if (num2 > num1)
+            {
+                lblmayor.Text = num2.ToString();
+                lblmenor.Text = num1.ToString();
+                lblmayor.Visible = true;
+                lblmenor.Visible = true;
+
+            }
+            else
+                if (num1 == num2)
+            {
+                MessageBox.Show("los dos digitos son iguales");
+                lblmayor.Visible = false;
+                lblmenor.Visible = false;
+            }
+        }
+    }
+}
